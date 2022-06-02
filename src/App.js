eda844f693414760c5e6f7b768f7027df707cc42
Jezwan <jezwan002@gmail.com>
2022-06-02 09:59:08 +0530

    finished counter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import {useState} from 'react'
 function App() {
+  const [count,setCount] = useState(0)
+  function upCount(){
+    setCount(count+1)
+  }
+  function downCount(){
+    setCount(count-1)
+  }
   return (
-    <h1> Hello World </h1>
+    <div className="counter">
+    <button onClick={upCount}> up </button>
+    <button onClick={downCount}> down </button>
+    <h1> counter: {count} </h1>
+    </div>
   );
 }
 
